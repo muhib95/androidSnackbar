c34d86805723342b07368bf7553f48c02e8a7dec
Muhibbul hasan <muhibbulhasan6@gmail.com>
2024-01-14 22:47:57 +0600

    add image from local

--- lib/main.dart
+++ lib/main.dart
@@ -3,47 +3,85 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-main(){
+main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return const MaterialApp(debugShowCheckedModeBanner: false,home: HomeActivity());
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomeActivity());
   }
-
 }
 
-class HomeActivity extends StatelessWidget{
+class HomeActivity extends StatelessWidget {
   const HomeActivity({super.key});
 
-
-  mySnackBar(msg,context){
-    return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(msg))
-    );
+  mySnackBar(msg, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(msg)));
   }
-  myEmail(msg,context){
-    return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(msg))
-    );
 
+  myEmail(msg, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(msg)));
   }
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold(appBar: AppBar(title:const Text("Show Message",style: TextStyle(color: Colors.white),) ,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Show Message",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+        iconTheme: const IconThemeData(color: Colors.white),
+        actions: [
+          IconButton(
+            onPressed: () {
+              mySnackBar("Message", context);
+            },
+            icon: const Icon(Icons.message),
+            color: Colors.white,
+          ),
+          IconButton(
+            onPressed: () {
+              myEmail("Email", context);
+            },
+            icon: const Icon(Icons.email),
+            color: Colors.white,
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: const [
+            DrawerHeader(
+                padding: EdgeInsets.all(0),
+                child: UserAccountsDrawerHeader(
+                  accountName: Text("Muhibbul Hasan"),
+                  accountEmail: Text("muhibbulhasan6@gmail.com"),
+                  decoration: BoxDecoration(color: Colors.lightBlueAccent),
 
-      backgroundColor: Colors.blue,
-  actions: [
-    IconButton(onPressed: (){mySnackBar("Message",context);}, icon:const Icon(Icons.message),color: Colors.white,),
-    IconButton(onPressed: (){myEmail("Email",context);}, icon:const Icon(Icons.email),color: Colors.white,),
-
-  ],
-  ),);
+                )),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+            ),
+            ListTile(
+              leading: Icon(Icons.contact_mail),
+              title: Text("Contact"),
+            )
+          ],
+        ),
+      ),
+      body: Center(
+        child: Image.asset("assets/images/download.jpeg")
+      ),
+    );
   }
-
 }
